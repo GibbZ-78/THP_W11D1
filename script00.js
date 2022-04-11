7a4c6434ec18ce3a7206e6783cc83b08a34329dc
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-04-11 15:11:15 +0200

    Very Ast commit after exercise #1

---
+++ script00.js
@@ -0,0 +1,17 @@
+let myValue = document.getElementById("myDDL").value;
+switch (key) {
+  case "1":
+    console.log
+    break;
+  case "2":
+
+    break;
+  case "3":
+
+    break;
+}
+
+if (true) {
+  const module = await import('./script01.js')
+  module.addNumbers(3, 4, 5)
+}
